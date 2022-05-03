22ab77d756cb6af53fc8b889c85d90d2bf687717
Tomas74581 <89972320+Tomas74581@users.noreply.github.com>
2022-05-03 11:59:14 +0000

    Revert "Base"
    This reverts commit 924d7ef58abecaa7e3a4082c56b4523df393342a.

--- Profile/views.py
+++ Profile/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-    return render(request, 'Profile/Index.html')
+def index(response):
+    return HttpResponse('<h1>Hello from the index view in the Profile app</h1>')
--- items/views.py
+++ items/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'items/Index.html')
+    return HttpResponse('<h1>Öskur trúðsins inní nóttinni</h1>')
