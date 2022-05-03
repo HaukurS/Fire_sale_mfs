6fcd600051ac8caca99ee2fe26b59958a672935c
HaukurS <89972635+HaukurS@users.noreply.github.com>
2022-05-03 11:03:25 +0000

    items app

--- Fire_sale_mfs/urls.py
+++ Fire_sale_mfs/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Profile/', include('Profile.urls'))
+    path('Profile/', include('Profile.urls')),
+    path('items/', include('items.urls'))
 ]
