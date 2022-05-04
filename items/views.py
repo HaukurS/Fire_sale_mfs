788925a500bcbeeef127969d1a07c3168e1be70f
raggiolafs00 <ragnaro21@ru.is>
2022-05-04 11:26:25 +0000

    forms added to items and database added to settings

--- items/views.py
+++ items/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from items.forms.item_form import ItemCreateForm
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'Item/Index.html')
 
@@ -10,6 +12,9 @@ def create_item(request):
     if request.method == 'POST':
         print(1)
     else:
-        print(2)
+        form = ItemCreateForm()
         # TODO: Instance new ItemCreateForm()
+    return render(request, 'candy/create_candy.html', {
+        'form': form
+    })
 
