2568abbd1bfff6712fae080e82ba42c50de9f682
Tomas74581 <89972320+Tomas74581@users.noreply.github.com>
2022-05-05 09:12:05 +0000

    Update views.py

--- homepage/views.py
+++ homepage/views.py
@@ -9,7 +9,7 @@ def home(request):
 def search_item(request):
     if request.method == "POST":
         searched = request.POST['searched']
-        items = Item.objects.filter(name__contains=searched)
+        items = Item.objects.filter(name__icontains=searched)
         return render(request, 'HomePage/Search_item.html',
                       {'searched':searched,
                        'items':items})
