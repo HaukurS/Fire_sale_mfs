c5e4a23a3ba60b95f5f9b49240c6945e2d578673
HaukurS <89972635+HaukurS@users.noreply.github.com>
2022-05-05 17:47:06 +0000

    footer

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    # localhost:8000
+    # localhost:8000/home
     path('', views.home, name="homepage"),
     path('search_item', views.search_item, name="itemsearch")
 ]
