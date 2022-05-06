97e9181f6174c8f8c30ab2ae7131a0a10da51215
raggiolafs00 <ragnaro21@ru.is>
2022-05-06 10:18:49 +0000

    checkout urls added

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
+from Users.models import User
+from checkout.form.checkout_form1 import ContactCreateForm
 from checkout.form.checkout_form2 import PaymentInfoCreateForm
 
 
@@ -12,6 +14,21 @@ def create_paymentinfo(request):
             return redirect('index')
     else:
         form = PaymentInfoCreateForm()
+        # TODO: Instance new ItemCreateForm()
+    return render(request, 'Item/create_item.html', {
+        'form': form
+    })
+
+
+def create_contactinfo(request, id):
+    instance = get_object_or_404(User, pk=id)
+    if request.method == 'POST':
+        form = ContactCreateForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = ContactCreateForm(instance=instance)
         # TODO: Instance new ItemCreateForm()
     return render(request, 'Item/create_item.html', {
         'form': form
