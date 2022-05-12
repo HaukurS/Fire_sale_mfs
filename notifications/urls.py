856c38344f1a42b597f95d5e6dc9d478a26fbb6a
Tomas74581 <89972320+Tomas74581@users.noreply.github.com>
2022-05-12 10:57:48 +0000

    comments

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-
+#urlpatterns that connect to the notifications
 urlpatterns = [
     # localhost:8000/notifications
     path('delete/<int:id>', views.delete_notification, name='delete'),
